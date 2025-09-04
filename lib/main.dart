7cdbb7a9d29bdb7804b3c7197c5d329fe8f6a331
Mualip Suhal <mualipsuhal@gmail.com>
2025-09-04 16:07:31 +0700

    build(deps): migrate to hive_ce_flutter for flutter support
    Update Hive initialization to use flutter-specific package and add hive_ce_flutter dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flx_nocode_flutter/src/app/model/configuration.dart';
 import 'package:flx_core_flutter/flx_core_flutter.dart';
 import 'package:flx_nocode_flutter/src/app/model/entity.dart';
@@ -7,16 +8,12 @@ import 'package:flutter/material.dart';
 import 'package:flx_nocode_flutter/src/app/view/page/landing/landing_page.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flx_authentication_flutter/flx_authentication_flutter.dart';
-import 'package:hive_ce/hive.dart';
-import 'package:path_provider/path_provider.dart';
-
+import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'src/app/view/page/entity_create/entity_create_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  final path = (await getApplicationDocumentsDirectory());
-  Hive.init(path.path);
+  await Hive.initFlutter();
 
   await Configuration.load();
   final configuration = Configuration.instance;
