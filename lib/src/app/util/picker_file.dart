df93012a1bb3b05ca607f872c6c669f5d45a67c1
Mualip Suhal <mualipsuhal@gmail.com>
2025-10-23 13:14:01 +0700

    refactor(file_handling): simplify file picker and web file saver
    Remove redundant file extension and clean up web file saver implementation

--- lib/src/app/util/picker_file.dart
+++ lib/src/app/util/picker_file.dart
@@ -3,17 +3,15 @@ import 'package:file_picker/file_picker.dart';
 import 'save_file_io.dart' if (dart.library.html) 'save_file_web.dart' as saver;
 
 Future<FilePickerResult?> pickFile({
-  List<String>? extensions,
+  List<String>? file,
   FileType type = FileType.any,
-  bool allowMultiple = false,
 }) async {
   final effectiveType =
-      (extensions != null && extensions.isNotEmpty) ? FileType.custom : type;
+      (file != null && file.isNotEmpty) ? FileType.custom : type;
 
   final result = await FilePicker.platform.pickFiles(
-    allowedExtensions: extensions,
+    allowedExtensions: file,
     type: effectiveType,
-    allowMultiple: allowMultiple,
     withData: true,
   );
   return result;
--- lib/src/app/util/save_file_web.dart
+++ lib/src/app/util/save_file_web.dart
@@ -4,10 +4,6 @@ import 'dart:html';
 void saveFile(List<int> bytes, String filename) {
   final dataUrl =
       'data:application/octet-stream;base64,${base64.encode(bytes)}';
-  final anchor = AnchorElement(href: dataUrl)
-    ..download = filename
-    ..style.display = 'none';
-  document.body?.append(anchor);
-  anchor.click();
-  anchor.remove();
+  final a = AnchorElement(href: dataUrl)..download = filename;
+  a.click();
 }
