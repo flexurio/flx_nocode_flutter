e4c11f2f80acc8500553b3e72306296ea86316b9
Mualip Suhal <mualipsuhal@gmail.com>
2026-03-04 13:10:25 +0700

    refactor: extract `_TimeFormatter` into a new `TimeFormatter` utility file.

--- lib/features/component/screen/widgets/component_time_field.dart
+++ lib/features/component/screen/widgets/component_time_field.dart
@@ -4,6 +4,7 @@ import 'package:flx_core_flutter/flx_core_flutter.dart';
 import 'package:flx_nocode_flutter/features/component/models/component_time_field.dart';
 import 'package:flx_nocode_flutter/features/layout_form/models/layout_form.dart';
 import 'package:flx_nocode_flutter/core/utils/js/string_js_interpolation.dart';
+import 'package:flx_nocode_flutter/core/utils/time_formatter.dart';
 
 extension ComponentTimeFieldWidgets on ComponentTimeField {
   Widget toWidget(JsonMap data) {
@@ -29,7 +30,7 @@ extension ComponentTimeFieldWidgets on ComponentTimeField {
       hintText: '00:00',
       inputFormatters: [
         FilteringTextInputFormatter.allow(RegExp(r'[0-9:]')),
-        _TimeFormatter(),
+        TimeFormatter(),
       ],
       validator: this.required
           ? (value) {
@@ -84,52 +85,6 @@ extension ComponentTimeFieldWidgets on ComponentTimeField {
           ),
         ),
       ],
-    );
-  }
-}
-
-class _TimeFormatter extends TextInputFormatter {
-  @override
-  TextEditingValue formatEditUpdate(
-    TextEditingValue oldValue,
-    TextEditingValue newValue,
-  ) {
-    String text = newValue.text;
-
-    if (text.length > 5) return oldValue;
-
-    // Handle backspace on colon
-    if (oldValue.text.endsWith(':') &&
-        text.length == 2 &&
-        oldValue.text.length == 3) {
-      text = text.substring(0, 1);
-    }
-
-    final buffer = StringBuffer();
-    for (int i = 0; i < text.length; i++) {
-      final char = text[i];
-      if (char == ':') continue;
-
-      final currentLen = buffer.length;
-      if (currentLen == 0) {
-        if (int.parse(char) > 2) continue;
-      } else if (currentLen == 1) {
-        final firstHourDigit = int.parse(buffer.toString());
-        if (firstHourDigit == 2 && int.parse(char) > 3) continue;
-      } else if (currentLen == 3) {
-        if (int.parse(char) > 5) continue;
-      }
-
-      buffer.write(char);
-      if (buffer.length == 2) {
-        buffer.write(':');
-      }
-    }
-
-    final newText = buffer.toString();
-    return TextEditingValue(
-      text: newText,
-      selection: TextSelection.collapsed(offset: newText.length),
     );
   }
 }
