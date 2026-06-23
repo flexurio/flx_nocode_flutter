0782c9bbc426f095f32acd6776f0017e87cf8e67
Mualip Suhal <mualipsuhal@gmail.com>
2026-06-23 10:17:40 +0700

    refactor: update scale animation implementation in configuration error page to use diagonal3Values matrix

--- lib/features/configuration/screen/pages/configuration_error_page.dart
+++ lib/features/configuration/screen/pages/configuration_error_page.dart
@@ -406,7 +406,7 @@ class _AnimatedButtonState extends State<_AnimatedButton> {
       onExit: (_) => setState(() => _isHovered = false),
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 200),
-        transform: Matrix4.identity()..scaleByDouble(_isHovered ? 1.05 : 1.0),
+        transform: Matrix4.diagonal3Values(_isHovered ? 1.05 : 1.0, _isHovered ? 1.05 : 1.0, 1.0),
         child: Container(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(12),
