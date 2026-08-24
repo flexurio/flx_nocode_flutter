5386ee562132b88f0651184e3f90374bdf081a62
abdurrahman arifin <abdurrahman.arifin189@gmail.com>
2026-08-24 11:08:12 +0700

    fix(util): implement saveFileNocodeCore for web build compatibility

--- lib/src/app/util/save_file_web.dart
+++ lib/src/app/util/save_file_web.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:html';
 
-void saveFile(List<int> bytes, String filename) {
+void saveFileNocodeCore(List<int> bytes, String filename) {
   final dataUrl =
       'data:application/octet-stream;base64,${base64.encode(bytes)}';
   final a = AnchorElement(href: dataUrl)..download = filename;
