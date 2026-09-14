6a5817fdca25dfad3cab549b3a0dde9fe023c245
Mualip Suhal <mualipsuhal@gmail.com>
2026-09-14 21:42:39 +0700

    feat: add onChangeActions and dependsOn support to ComponentNumberField model and widget

--- lib/features/component/models/component_number_field.dart
+++ lib/features/component/models/component_number_field.dart
@@ -1,14 +1,17 @@
+import 'package:flx_nocode_flutter/features/component/models/component_action.dart';
 import 'package:flx_nocode_flutter/features/component/models/component_input_base.dart';
 import 'package:flx_nocode_flutter/features/component/models/component_size_mode.dart';
 import 'package:flx_nocode_flutter/features/layout_form/models/layout_form.dart';
 
 class ComponentNumberField extends ComponentInputBase {
   final String initialValue;
+  final List<ComponentAction> onChangeActions;
 
   ComponentNumberField({
     required super.id,
     required super.label,
     required this.initialValue,
+    this.onChangeActions = const [],
     super.enabled = true,
     super.required,
     super.widthMode,
@@ -16,6 +19,7 @@ class ComponentNumberField extends ComponentInputBase {
     super.flex,
     super.visibilityCondition,
     super.events,
+    super.dependsOn,
   }) : super(type: componentId);
 
   static const String componentId = 'number_field';
@@ -46,10 +50,35 @@ class ComponentNumberField extends ComponentInputBase {
       return true;
     }();
 
+    final rawActions = map['onChangeActions'] ?? map['on_change'];
+    final onChangeActions = <ComponentAction>[];
+    if (rawActions is List) {
+      for (final item in rawActions) {
+        if (item is Map<String, dynamic>) {
+          onChangeActions.add(ComponentAction.fromMap(item));
+        }
+      }
+    } else if (rawActions is Map) {
+      onChangeActions.add(
+        ComponentAction.fromMap(Map<String, dynamic>.from(rawActions)),
+      );
+    }
+
+    final rawDependsOn = map['dependsOn'];
+    final dependsOn = <String>[];
+    if (rawDependsOn is List) {
+      for (final item in rawDependsOn) {
+        if (item != null) {
+          dependsOn.add(item.toString());
+        }
+      }
+    }
+
     return ComponentNumberField(
       id: id,
       label: label,
       initialValue: initialValue,
+      onChangeActions: onChangeActions,
       enabled: enabled,
       required: map['required'] == true,
       widthMode: ComponentSizeMode.fromString(map['widthMode']?.toString()),
@@ -57,6 +86,7 @@ class ComponentNumberField extends ComponentInputBase {
       flex: int.tryParse(map['flex']?.toString() ?? ''),
       visibilityCondition: map['visibilityCondition']?.toString(),
       events: map['events'] as Map<String, dynamic>? ?? const {},
+      dependsOn: dependsOn,
     );
   }
 
@@ -64,5 +94,6 @@ class ComponentNumberField extends ComponentInputBase {
   JsonMap toMap() => {
         ...super.toMap(),
         'initialValue': initialValue,
+        'onChangeActions': onChangeActions.map((a) => a.toMap()).toList(),
       };
 }
--- lib/features/component/screen/widgets/component_number_field.dart
+++ lib/features/component/screen/widgets/component_number_field.dart
@@ -2,13 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flx_core_flutter/flx_core_flutter.dart';
 import 'package:flx_nocode_flutter/features/component/models/component_number_field.dart';
+import 'package:flx_nocode_flutter/core/utils/js/string_js_interpolation.dart';
 
 extension ComponentNumberFieldWidgets on ComponentNumberField {
   Widget toWidget(Map<String, dynamic> data) {
+    final allControllers = data['allControllers'] as Map<String, TextEditingController>?;
     final controller = data['controller'] as TextEditingController? ??
-        (data['allControllers'] != null
-            ? (data['allControllers'] as Map<String, TextEditingController>)[id]
-            : null);
+        (allControllers != null ? allControllers[id] : null);
+
+    void executeChangeActions(String val) {
+      if (onChangeActions.isEmpty) return;
+
+      final controllerValues = <String, dynamic>{};
+      if (allControllers != null) {
+        for (final entry in allControllers.entries) {
+          controllerValues[entry.key] = entry.value.text;
+        }
+      }
+
+      final context = <String, dynamic>{
+        ...data,
+        ...controllerValues,
+        'value': val,
+        'val': val,
+        id: val,
+        'form': controllerValues,
+      };
+
+      for (final action in onChangeActions) {
+        if (action.type == 'set_value') {
+          final targetId = action.targetId;
+          final targetCtrl = allControllers?[targetId];
+          if (targetCtrl != null) {
+            final rawValue = action.value ?? '';
+            final calculatedValue = rawValue.interpolateJavascript(context);
+            if (targetCtrl.text != calculatedValue) {
+              targetCtrl.text = calculatedValue;
+            }
+          }
+        }
+      }
+    }
+
     return FTextFormField(
       controller: controller,
       labelText: label,
@@ -18,6 +53,9 @@ extension ComponentNumberFieldWidgets on ComponentNumberField {
       inputFormatters: [
         FilteringTextInputFormatter.allow(RegExp(r'[0-9.]')),
       ],
+      onChanged: (val) {
+        executeChangeActions(val);
+      },
     );
   }
 
